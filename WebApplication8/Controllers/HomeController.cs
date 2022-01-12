2fb4aeea9f61349fe7e875f93c53ca7c166ff2c5
shubham saini <shubham2016@outlook.com>
2022-01-12 16:58:55 +0530

    V1

--- WebApplication8/Controllers/HomeController.cs
+++ WebApplication8/Controllers/HomeController.cs
@@ -23,6 +23,12 @@ namespace WebApplication8.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
